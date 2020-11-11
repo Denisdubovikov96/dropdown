ab7c87fdb0d738855500e2d5e850f36be2917112
Dmytro Sych <sychdo@gmail.com>
2020-11-11 13:07:11 +0200

    fix cors

--- server/index.js
+++ server/index.js
@@ -1,6 +1,9 @@
 const express = require("express");
 const app = express();
 const data = require("./data.json");
+const cors = require("cors");
+
+app.use(cors());
 
 app.get("/", (req, res) => {
   return res.json(data);
